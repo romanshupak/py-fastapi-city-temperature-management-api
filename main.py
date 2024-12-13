ad376ddee61ce936d52545224fd973eef4d05335
Roman Shupak <romanshupak808@gmail.com>
2024-12-13 17:18:07 +0200

    Create main py.

---
+++ main.py
@@ -0,0 +1,20 @@
+from fastapi import FastAPI
+from sqlalchemy.orm import Session
+
+from database import SessionLocal
+
+app = FastAPI()
+
+
+def get_db() -> Session:
+    db = SessionLocal()
+
+    try:
+        yield db
+    finally:
+        db.close()
+
+
+@app.get("/")
+def root():
+    return {"message": "Hello World"}
