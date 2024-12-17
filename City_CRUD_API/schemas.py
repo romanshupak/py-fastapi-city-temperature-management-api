93c3ae66fabb70c80de098219dc99a29a52c5ff7
Roman Shupak <romanshupak808@gmail.com>
2024-12-17 15:37:34 +0200

    Create schemas for cities.

---
+++ City_CRUD_API/schemas.py
@@ -0,0 +1,35 @@
+from typing import Optional
+
+from pydantic import BaseModel
+
+
+class CityBase(BaseModel):
+    name: str
+    additional_info: str
+
+
+class CityCreate(CityBase):
+    pass
+
+
+class CityList(CityBase):
+    id: int
+
+    class Config:
+        from_attributes = True
+
+
+class CityDelete(CityBase):
+    id: int
+
+    class Config:
+        from_attributes = True
+
+
+class CityUpdate(CityBase):
+    id: int
+    name: Optional[str] = None
+    additional_info: Optional[str] = None
+
+    class Config:
+        from_attributes = True
