461e7f215766b9251bde628ee0473e4af6622061
Roman Shupak <romanshupak808@gmail.com>
2024-12-18 16:24:59 +0200

    Include temperature router.

--- main.py
+++ main.py
@@ -2,11 +2,13 @@ from fastapi import FastAPI
 from sqlalchemy.orm import Session
 
 from City_CRUD_API import router as city_router
+from Temperature_API import router as temperature_router
 from database import SessionLocal
 
 app = FastAPI()
 
 app.include_router(city_router.router)
+app.include_router(temperature_router.router)
 
 
 @app.get("/")
